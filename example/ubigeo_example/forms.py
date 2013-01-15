f6df8ed025c7ffc7192460db7ab9ec6f23ef8f40
micky miseck <themiseck.rock@gmail.com>
2013-01-15 18:26:01 -0500

    fix load ubigeo in the widget, PuecoPop you need to merge

--- example/ubigeo_example/forms.py
+++ example/ubigeo_example/forms.py
@@ -2,6 +2,13 @@
 
 from django import forms
 from ubigeo.fields import UbigeoField
+from .models import Incident
 
 class DisplayUbigeosForm(forms.Form):
     ubigeo = UbigeoField(required=False)
+
+class DetailUbigeoForm(forms.ModelForm):
+
+    location = UbigeoField(required=False)
+    class Meta:
+        model = Incident
