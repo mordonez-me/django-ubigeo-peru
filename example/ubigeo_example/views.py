89056f811b94d6aa0de30aa796ba58a497c9b891
Javier Olaechea <pirata@gmail.com>
2013-01-17 13:11:50 -0500

    Resuse template and redirect to recently saved template

--- example/ubigeo_example/views.py
+++ example/ubigeo_example/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from .forms import DisplayUbigeosForm, DetailUbigeoForm
 from .models import Incident
@@ -11,8 +12,11 @@ def home(request):
     if request.method == "POST":
         form = DisplayUbigeosForm(request.POST)
         if form.is_valid():
-            Incident(location=form.cleaned_data['ubigeo']).save()
-            return HttpResponse("A-OK")
+            incident = Incident(location=form.cleaned_data['ubigeo'])
+            incident.save()
+            return redirect(
+                reverse('incident_detail',
+                        kwargs={'incident_id': incident.pk, }))
         else:
             return render(request,
                           'index.html',
@@ -31,6 +35,6 @@ def incident_detail(request, incident_id):
     incident = get_object_or_404(Incident,pk=incident_id)
     form = DetailUbigeoForm(instance=incident)
     return render(request,
-        'incident_detail.html',
+        'index.html',
         {'form': form,
         'incidents': Incident.objects.all()}, )
